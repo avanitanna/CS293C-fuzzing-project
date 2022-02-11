f1c40fa3f9922129b260e387218c4b9fcb5534ee
Avani Tanna <33663827+avanitanna@users.noreply.github.com>
2022-02-11 08:52:11 -0800

    create json grammar in python from antlr
    use antlr and create json grammar in python using the fuzzing book format

---
+++ json_grammar.py
@@ -0,0 +1,52 @@
+## json grammar from antlr in python 
+## CITE: https://github.com/antlr/grammars-v4/blob/master/json/JSON.g4, Fuzzing book 
+
+JSON_GRAMMAR = {
+    "<json>":
+        ["<value>"],
+
+    "<obj>":
+        ["<'{' pair (',' pair)* '}'>",  "<'{' '}'>"],
+
+    "<pair>":
+        ["<STRING ':' value>"],
+
+    "<arr>":
+        ["<'[' value (',' value)* ']'>", "<'[' ']'>"],
+    
+    "<value>":
+        ["<STRING>", "<NUMBER>", "<obj>", "<arr>", "<'true'>", "<'false'>", "<'null'>"], 
+
+    "<STRING>":
+        ["<'"' (ESC | SAFECODEPOINT)* '"'>"],
+
+    "<fragment ESC>":
+        ["<"+"'\\' (["+'"\\/bfnrt] | UNICODE)'+">"],
+   
+    "<fragment UNICODE>":
+        ["<'u' HEX HEX HEX HEX>"],
+
+    "<fragment HEX>":
+        ["<0-9a-fA-F>"],
+   
+    "<fragment SAFECODEPOINT>":
+        ["<"+"~ ["+'"\\\u0000-\u001F]'+">"],
+
+    "<NUMBER>":
+        ["<'-'? INT ('.' [0-9] +)? EXP?>"],
+  
+    "<fragment INT>":
+        ["<'0' | [1-9] [0-9]*>"],
+   
+    # no leading zeros
+
+    "<fragment EXP>":
+      ["<[Ee] [+\-]? INT>"],
+   
+    # \- since - means "range" inside [...]
+
+    "<WS>":
+      ["<[ \t\n\r] + -> skip>"]
+
+
+} 
